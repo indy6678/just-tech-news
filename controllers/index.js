189b19ad1e2f035eee53d88f932566c61988d2e0
indy6678 <indy6678@gmail.com>
2022-08-26 22:09:16 -0500

    set up homepage

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,11 @@
 // collect packaged API routes
-
+const homeRoutes = require('./home-routes');
 const router = require('express').Router();
 
 const apiRoutes = require('./api/index');
 
 router.use('/api', apiRoutes); // all routes will  be prefixed with /api
+router.use('/', homeRoutes);
 
 // this is used to capture any endpoint requests that do not exist
 router.use((req, res) => {
