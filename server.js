5214d0453b367676d9b5030c32380c7fcd574254
indy6678 <indy6678@gmail.com>
2022-08-28 20:18:44 -0500

    added helpers

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require("express");
 const routes = require("./controllers/");
 const sequelize = require("./config/connection"); // import connection to Sequelize
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({helpers});
 const session = require('express-session');
+
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
