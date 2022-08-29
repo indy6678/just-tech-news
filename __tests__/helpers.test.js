00e9c026c74cf86be8f6173de81682f9ecd87abc
indy6678 <indy6678@gmail.com>
2022-08-28 20:07:20 -0500

    added url tests

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,5 +1,6 @@
 const {format_date} = require('../utils/helpers');
-const {format_plural} = require('../utils/helpers')
+const {format_plural} = require('../utils/helpers');
+const {format_url} = require('../utils/helpers');
 
 test('format_date() returns a date string', () => {
     const date = new Date('2020-03-20 16:12:03');
@@ -8,6 +9,17 @@ test('format_date() returns a date string', () => {
 });
 
 test('format_plural() returns a pluralized word', () => {
+    const plural = (format_plural('tiger', 2))
    
-    expect(format_plural('tiger', 2)).toBe('tigers');
-})
+    expect(plural).toBe('tigers');
+});
+
+test('format_url() returns a simplified url string', () => {
+    const url1 = format_url('http://test.com/page/1');
+    const url2 = format_url('https://www.coolstuff.com/abcdefg/');
+    const url3 = format_url('https://www.google.com?q=hello');
+
+    expect(url1).toBe('test.com');
+    expect(url2).toBe('coolstuff.com');
+    expect(url3).toBe('google.com');
+});
